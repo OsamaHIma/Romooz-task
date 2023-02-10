4e2acd3833b9b36394dae136f31e9b4edc155e54
Osama magdy <108794970+OsamaHIma@users.noreply.github.com>
2023-02-10 16:14:31 +0200

    push

--- src/Routes/projects/projects.jsx
+++ src/Routes/projects/projects.jsx
@@ -102,29 +102,6 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="col-md-3 my-1">
-            <div className="project-img my-4">
-              <img src="/images/restaurant-interior.jpg" alt="" />
-            </div>
-          </div>
-          <div className="col-md-8 mt-1 projects-details">
-            <div className="project-details">
-              <p>
-                <strong>هذا النص مهم للمشاريع</strong>
-              </p>
-              <div className="d-flex">
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Animi laboriosam esse odio soluta autem, culpa quaerat ipsa
-                  quis, ea, sed minus. Quidem accusantium fugit laboriosam qui
-                  inventore, quam, impedit consequuntur reprehenderit officia
-                  explicabo maiores autem sed debitis. Similique, earum cumque.
-                  Lorem ipsum dolor sit amet.
-                </p>
-                <button className="btn mx-2">صور المشاريع</button>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       <div className="learn-more">
